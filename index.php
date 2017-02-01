fa6835a1d914d5b564e01f2e8a7d343e99b5418e
Cphil31 <cecile_philippe@live.fr>
2017-02-01 16:22:23 +0100

    step 1 fait

--- index.php
+++ index.php
@@ -3,30 +3,30 @@ require_once 'vendor/autoload.php';
 ORM::configure('mysql:host=localhost;dbname=my_blog');
 ORM::configure('username','root');
 ORM::configure('password','root');
- ?>
- <!DOCTYPE html>
- <html lang="en">
- <head>
- 	<meta charset="UTF-8">
- 	<title>Document</title>
- </head>
- <body>
- <h1>Mon blog </h1>
- 	<table>
-
-
-		<?php 
-
-		$tab = ORM::for_table('posts')->find_many();
-		foreach ($tab as $tableau) {
-
-			echo $tableau->id.$tableau->title.'</br>';
-			
-		}
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
+</head>
+<body>
+	<h1>Mon blog </h1>
 	
-	 ?>
+		<table class="ui celled table">
+			<?php 
+
+			$tab = ORM::for_table('posts')->find_many();
+			foreach ($tab as $tableau) {
+
+				echo $tableau->id.$tableau->title." ".$tableau->author." ".$tableau->content." ".$tableau->created_at." ".$tableau->updated_at.'</br>';
+
+			}
+
+			?>
+
+		</table> 
 	
-	</table> 
- </body>
- </html> 
+</body>
+</html> 
